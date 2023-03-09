da3e7acdcf92ce369f3d5d248a07ec6dc5fca326
xeviert <xevierturrubiartes@gmail.com>
2023-03-09 19:57:27 +0300

    removed db

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL || '',
   JWT_SECRET: process.env.JWT_SECRET || 'my-little-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 };
